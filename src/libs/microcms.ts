08a6d50c99d2fc9794a8caa4c297d0da1a7e1fcc
Yamahara Yoshihiro <yoshihiro@yamahara.email>
2025-01-09 22:00:53 +0900

    日付の日本語化とmicrocms.tsの型付け

--- src/libs/microcms.ts
+++ src/libs/microcms.ts
@@ -1,12 +1,18 @@
-import { createClient, type MicroCMSQueries } from "microcms-js-sdk";
+import type { MicroCMSListContent, MicroCMSQueries } from "microcms-js-sdk";
+import { createClient } from "microcms-js-sdk";
 
 const client = createClient({
   serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
   apiKey: import.meta.env.MICROCMS_API_KEY,
 });
 
-export const getBlogs = async (endpoint: string, queries: MicroCMSQueries) => {
-  return await client.get({ endpoint: endpoint, queries });
+export type Blog = {
+  title: string;
+  content: string;
+} & MicroCMSListContent;
+
+export const getBlogs = async (endpoint: string, queries?: MicroCMSQueries) => {
+  return await client.getList<Blog>({ endpoint: endpoint, queries });
 };
 
 export const getBlogDetail = async (endpoint: string, blogId: string, queries?: MicroCMSQueries) => {
