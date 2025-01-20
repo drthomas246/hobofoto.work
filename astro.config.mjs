e19a1b9b459c3b25e774122ef6702f6c2c98377c
Yamahara Yoshihiro <yoshihiro@yamahara.email>
2025-01-20 19:02:14 +0900

    sitemapが作成されない不具合に対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import react from '@astrojs/react';
+import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
 import rehypeMathML from '@daiji256/rehype-mathml';
 import { defineConfig } from 'astro/config';
 import remarkDirective from "remark-directive";
 import remarkMath from 'remark-math';
-import sitemap from "./integrations/sitemap";
 import remarkRubyDirective from "./src/plugins/remarkRuby";
 import remarkRubyStringDirective from "./src/plugins/remarkRubyString";
 import remarkTalkingLeftDirective from "./src/plugins/remarkTalkingLeft";
@@ -14,7 +14,17 @@ import remarkVimeoDirective from "./src/plugins/remarkVimeo";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), sitemap()],
+  site: "https://www.hobofoto.work/",
+  integrations: [
+    react(),
+    sitemap(
+      {
+        filter: (page) =>
+          page !== "https://www.hobofoto.work/aboutme/confirm/" &&
+          page !== "https://www.hobofoto.work/aboutme/thanks/"
+      }
+    )
+  ],
 
   markdown: {
     remarkPlugins: [
